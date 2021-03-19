bf348f38da7968f25c047bfa82092c444f724f8a
Kyle <kjfossman@gmail.com>
2021-03-19 15:13:27 -0800

    commit tree 0c69b9658635cef5afefc4cc643166f18db4037a

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_192521) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.decimal "balance"
+    t.decimal "balance", precision: 1
   end
 
 end
